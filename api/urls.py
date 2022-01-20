36ba2d3128f40dc869e4d3fe73d7a5fb94200c54
majid-757 <majid.asadollahi95@gmail.com>
2022-01-20 23:55:22 +0330

    start drf

---
+++ api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from .views import ArticleList
+
+app_name = 'api'
+
+
+urlpatterns = [
+    path('', ArticleList.as_view(), name='list'),
+]
+
+
+
+
+
+
---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import Article
+
+
+class ArticleList(ListView):
+    def get_queryset(self):
+        return Article.objects.filter(status=True)
+
+
